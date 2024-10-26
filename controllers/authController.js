fc522b95197810d276634918c5e1182c4448a315
MoonSsunny <moonsun116@naver.com>
2024-10-26 14:08:35 +0900

    [feat] 동기화 작업 수정

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,23 +6,25 @@ authController.loginEmail = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email }, '-createdAt -updatedAt -__v');
-    if (user) {
-      const isMatch = bcrypt.compare(password, user.password);
-      if (isMatch) {
-        const token = await user.generateToken();
-        return res.status(200).json({ status: 'success', user, token });
-      } else {
-        const error = new Error('아이디 또는 비밀번호가 없습니다');
-        error.status = 401;
-        return error;
-      }
-    } else {
+
+    if (!user) {
       const error = new Error('가입된 유저가 아닙니다.');
       error.status = 404;
-      return error;
+      return next(error);
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      const error = new Error('아이디 또는 비밀번호가 잘못되었습니다');
+      error.status = 401;
+      return next(error);
+    }
+
+    const token = await user.generateToken();
+    return res.status(200).json({ status: 'success', user, token });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     next(error);
   }
 };
