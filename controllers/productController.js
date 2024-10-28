86f4d4781b1f9305a5b61e899a5e91aba6d2f472
MoonSsunny <moonsun116@naver.com>
2024-10-28 13:48:23 +0900

    [feat] 페이징 api 작업

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/Product');
 
 const productController = {};
+const PAGE_SIZE = 5;
 
 productController.createProduct = async (req, res, next) => {
   try {
@@ -36,14 +37,27 @@ productController.createProduct = async (req, res, next) => {
 
 productController.getProduct = async (req, res, next) => {
   try {
-    const product = await Product.find({});
+    const { page, name } = req.query;
+    const cond = name ? { name: { $regex: name, $options: 'i' } } : {};
+    let response = { status: 'success' };
+    let query = Product.find(cond);
 
-    if (!product) {
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      const totalItemNum = await Product.countDocuments(cond);
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.total = totalPageNum;
+    }
+
+    const productList = await query.exec();
+    response.data = productList;
+
+    if (!productList || productList.length === 0) {
       const error = new Error('제품이 없습니다.');
       error.status = 404;
       return next(error);
     }
-    res.status(200).json({ status: 'success', data: product });
+    res.status(200).json(response);
   } catch (error) {
     console.log(error);
     next(error);
