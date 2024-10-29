f78a90efc6cce42bcc560cdb1603e7b793d4c1e5
MoonSsunny <moonsun116@naver.com>
2024-10-30 02:14:29 +0900

    [feat] 삭제리스트 볼수있는 api 추가

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,37 @@
+const { query } = require('express');
 const Product = require('../models/Product');
 
 const productController = {};
 const PAGE_SIZE = 5;
+
+const setDeleteStatus = async (productId, isDeleted) => {
+  return await Product.findByIdAndUpdate(
+    productId,
+    { isDeleted },
+    { new: true }
+  );
+};
+
+const getProduct = async (isDeleted = false, page, name = '') => {
+  const cond = { isDeleted };
+  if (name) {
+    cond.name = { $regex: name, $options: 'i' };
+  }
+  const query = Product.find(cond).sort({ createdAt: -1 });
+  let totalPageNum;
+
+  if (page) {
+    query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+    const totalItemNum = await Product.countDocuments(cond);
+    totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+  }
+  const productList = await query.exec();
+
+  return {
+    total: totalPageNum,
+    data: productList,
+  };
+};
 
 productController.createProduct = async (req, res, next) => {
   try {
@@ -46,27 +76,7 @@ productController.createProduct = async (req, res, next) => {
 productController.getProduct = async (req, res, next) => {
   try {
     const { page, name } = req.query;
-    const cond = { isDeleted: false };
-    if (name) {
-      cond.name = { $regex: name, $options: 'i' };
-    }
-
-    let response = { status: 'success' };
-    let query = Product.find(cond).sort({ createdAt: -1 });
-
-    if (page) {
-      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
-      const totalItemNum = await Product.countDocuments(cond);
-      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
-      response.total = totalPageNum;
-    }
-
-    const productList = await query.exec();
-    response.data = productList;
-
-    if (!productList || productList.length === 0) {
-      response.data = [];
-    }
+    const response = await getProduct(false, page, name);
     res.status(200).json(response);
   } catch (error) {
     console.log(error);
@@ -136,6 +146,17 @@ productController.deleteProduct = async (req, res, next) => {
     res
       .status(200)
       .json({ status: 'success', message: '삭제에 성공하였습니다.' });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+productController.getDeleteProduct = async (req, res, next) => {
+  try {
+    const { page, name } = req.query;
+    const response = await getProduct(true, page, name);
+    res.status(200).json(response);
   } catch (error) {
     console.log(error);
     next(error);
--- routes/productApi.js
+++ routes/productApi.js
@@ -25,4 +25,6 @@ router.delete(
   productController.deleteProduct
 );
 
+router.get('/delete', productController.getDeleteProduct);
+
 module.exports = router;
