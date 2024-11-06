307c6fa5a9f2010f9ebb9fae640d1413f31f845b
seonjuuuu <moonsun116@naver.com>
2024-11-06 18:35:39 +0900

    [feat] 카트 비우기 api 수정

--- models/Order.js
+++ models/Order.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('./User');
 const Product = require('./Product');
+const Cart = require('./Cart');
 const Schema = mongoose.Schema;
 
 const orderSchema = Schema(
@@ -56,7 +57,11 @@ orderSchema.methods.toJSON = function () {
   delete obj.updatedAt;
   return obj;
 };
-
+orderSchema.post('save', async function () {
+  const cart = await Cart.findOne({ userId: this.userId });
+  cart.items = [];
+  await cart.save();
+});
 const Order = mongoose.model('Order', orderSchema);
 
 module.exports = Order;
